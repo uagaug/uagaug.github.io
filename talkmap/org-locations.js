232df527d66f97a075239605ff7621be0d93f740
uagaug <zibochen@Zibos-MacBook-Pro.local>
2019-06-09 18:05:19 -0700

    talks

--- talkmap/org-locations.js
+++ talkmap/org-locations.js
@@ -1,7 +1,32 @@
 var addressPoints = [
   [
+    "Hangzhou, China",
+    30.2489634,
+    120.2052342
+  ],
+  [
+    "Beijing, China",
+    39.906217,
+    116.3912757
+  ],
+  [
     "San Ljubljana, Slovenia",
     46.0498146,
     14.5067824
+  ],
+  [
+    "Leavenworth, Washington",
+    47.5969138,
+    -120.6610816
+  ],
+  [
+    "Shanghai, China",
+    31.2252985,
+    121.4890497
+  ],
+  [
+    "Seattle, Washington",
+    47.6038321,
+    -122.3300624
   ]
 ];
